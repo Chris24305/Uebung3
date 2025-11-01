dbec6c5544de1db1e092445a176c5d3a68b38300
Chris24305 <christian.schaa@web.de>
2025-11-01 20:55:49 +0100

    H2erstellt

---
+++ src/h2/H2_main.java
@@ -0,0 +1,24 @@
+package h2;
+
+public class H2_main {
+	public static void main(String[] args) {
+		int jahr;
+		boolean schalt;
+		jahr = 2000;
+		schalt = false;
+		if (jahr % 4 == 0) {
+			if (jahr % 100 == 0) {
+				if (jahr % 400 == 0) {
+					schalt = true;
+				} else {
+					schalt = false;
+				}
+			} else {
+				schalt = true;
+			}
+		} else {
+			schalt = false;
+		}
+		System.out.print(schalt);
+	}
+}

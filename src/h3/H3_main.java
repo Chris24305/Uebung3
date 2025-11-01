63a7dfc3b63d69a9f78c66080b63ddc65695e3a0
Chris24305 <christian.schaa@web.de>
2025-11-01 21:45:18 +0100

    Final1

--- src/h3/H3_main.java
+++ src/h3/H3_main.java
@@ -4,5 +4,26 @@ public class H3_main {
 	public static void main(String[] args) {
 		int max, fix, wartend;
 		boolean istVoll;
+		max = 100;
+		fix = 101;
+		wartend = 200;
+		istVoll = false;
+		if (max - fix > 0) { 
+			if (max < fix + wartend) { 
+				wartend += fix - max;
+				fix = max;
+				istVoll = true;
+			} else {
+				fix += wartend;
+				wartend = 0;
+				if (max - fix == 0) {	
+					istVoll = true;
+				} else {
+					istVoll = false;
+				}
+			}
+		} else {
+			istVoll = true;
+		}
 	}
 }
